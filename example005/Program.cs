dd32d79e56d8dd2f2ccc93db40aa8548962f5cb0
Anna <anna5oclock@mail.ru>
2022-06-12 16:05:00 +0300

    Masha

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Wright username");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+   Console.WriteLine("My good girl"); 
+}
+else
+{
+   Console.Write("Hello, ");
+   Console.WriteLine(username);
+}
